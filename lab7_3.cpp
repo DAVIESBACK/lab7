4ecd466cef5f820863c173bc494f88d9d5e846dc
win <wwinnn478@gmail.com>
2023-12-15 05:18:55 +0700

    lab7

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
+ #include<iostream>
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    int i = 0;
+    while (i < 26){
+        if(  x == 'A'+i){
+            if(x == 'A'){
+                return 'Z';
+            }
+           return x-1 ; 
+        }
+        i++;
+    } 
+    return '0';
 }
 
 int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
-}
+    cout << before('Z');
+    return 0;
+} 
